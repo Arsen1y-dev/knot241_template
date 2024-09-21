95f00ef80484a494f24b8efa320b31ee29f74dfc
Arseniy Perfilov <129983483+Arsen1y-dev@users.noreply.github.com>
2024-09-21 10:25:48 +0400

    1 лаба 1 номер

---
+++ lab1_1.java
@@ -0,0 +1,14 @@
+public class lab1_1 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.print(i);
+
+            if (i % 10 != 0) {
+                System.out.print(", ");
+            } else {
+                System.out.println();
+            }
+        }
+    }
+}
